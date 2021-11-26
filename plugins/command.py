57d5e1192eca45794290629b06a035a9916922f6
AKHILESH KUMAR <48666821+akhilmeena@users.noreply.github.com>
2021-11-27 00:51:01 +0530

    update command.py

--- plugins/command.py
+++ plugins/command.py
@@ -1,22 +1,27 @@
-
-# (c) HeimanPictures
-
-
 import logging
+import os
+import requests
+from config import Config
+import pyrogram
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+import helper
+
+
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
-
-import os
-import requests
-
-from config import Config
-
-import pyrogram
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
-from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+@Client.on_message(filters.command('start') & filters.private)
+async def start(bot, message):
+  await message.reply_chat_action("typing")
+  await message.reply_text(
+    text=helper.STARTText.format(message.from_user.mention),
+    disable_web_page_preview=True,
+    reply_markup=START_BUTTONS
+    )
 
 @Client.on_message(filters.regex('http') & filters.private)
 async def pdisk(bot, message):
