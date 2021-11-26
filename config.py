66c99dbc63c0497c2c98ada3b577250390f7f3eb
AKHILESH KUMAR <48666821+akhilmeena@users.noreply.github.com>
2021-11-27 00:30:51 +0530

    update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 class Config(object):
     BOT_TOKEN = "2139115252:AAEYMEKzh33CGCqOxrq0sQTARf4-HgOkSjo"#os.environ.get("BOT_TOKEN", "")
-    APP_ID = 1687826#int(os.environ.get("APP_ID", ""))
+    API_ID = 1687826#int(os.environ.get("APP_ID", ""))
     API_HASH = "1af0252a7f97e288e2fff6b0adad5b2f"#os.environ.get("API_HASH", "")
     
 OWNER_ID = [818396979]
