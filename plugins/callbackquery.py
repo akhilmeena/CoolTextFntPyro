d12910a3105152aa8831ce005a58622755658f3a
AKHILESH KUMAR <48666821+akhilmeena@users.noreply.github.com>
2021-11-27 01:58:00 +0530

    update callbackquery.py

--- plugins/callbackquery.py
+++ plugins/callbackquery.py
@@ -19,17 +19,17 @@ async def cb_data(bot, update):
       disable_web_page_preview=True,
       reply_markup=helper.LBRYOPEN_BUTTONS
       )
-  elif update.data == "home2start":
+  if update.data == "home2start":
     await update.message.edit_text(
     text=helper.STARTText.format(update.from_user.mention),
     disable_web_page_preview=True,
     reply_markup=helper.START_BUTTONS
     )
-  elif update.data == "help":
+  if update.data == "help":
     await update.message.edit_text(
     text=helper.HELPTEXT,
     disable_web_page_preview=True,
     reply_markup=helper.HELP_BUTTONS
     )
-  else:# update.data == "close":
+  if update.data == "close":
     await update.message.delete()
