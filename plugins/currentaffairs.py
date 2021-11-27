96a39f535c62aae5b337b7bf380bc664b2ade91f
AKHILESH KUMAR <48666821+akhilmeena@users.noreply.github.com>
2021-11-27 16:36:54 +0530

    update currentaffairs.py

--- plugins/currentaffairs.py
+++ plugins/currentaffairs.py
@@ -4,6 +4,23 @@ from pyrogram.types import InlineKeyboardButton ,InlineKeyboardMarkup
 import string
 from itertools import islice
 
+
+VisionIas = InlineKeyboardButton('✨ Vision IAS', callback_data='crnafrsdaily')
+BackToLibrary = InlineKeyboardButton('🔙', callback_data='libraryopen')
+
+CRNTAFRSOURCEBTN = InlineKeyboardMarkup([
+  [VisionIas],
+  [BackToLibrary]
+  ])
+
+
+
+
+
+
+
+
+
 Btn = []
 
 @Client.on_message(filters.command('btn') & filters.private)
@@ -12,15 +29,6 @@ async def start(bot, message):
     x = InlineKeyboardButton(str(i),callback_data="akhil")
     Btn.append(x)
   ak = [Btn[i:i+3] for i in range(0, len(Btn), 3)]
-  #ak =  [Btn[i:i+3] for i in range(0,len(l),3)]
-#Out[17]: [[0, 1, 2], [3, 4, 5], [6, 7, 8], [9, 10, 11], [12, 13]]
-#Share Edit Follow
-  #ak = list(list(t) for t in zip(*[iter(Btn)]*3))
-  #print(ak)
-  #ak = list(chunk(Btn, 3))
-  #[(0, 1, 2), (3, 4, 5), (6, 7, 8), (9, 10, 11), (12, 13)]
-  #await message.reply_text(text=ak)
-  #await message.reply_text(text=LBRYOPEN_BUTTONS)
   START_BUTTONS = InlineKeyboardMarkup(ak)
   await message.reply_text(text=START_BUTTONS,reply_markup=START_BUTTONS)
    
