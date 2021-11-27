9ad446eb1ad40a3338c34b652777709c3641121f
AKHILESH KUMAR <48666821+akhilmeena@users.noreply.github.com>
2021-11-27 15:14:25 +0530

    update currentaffairs.py

--- plugins/currentaffairs.py
+++ plugins/currentaffairs.py
@@ -33,7 +33,7 @@ async def start(bot, message):
   for i in range(10):
     x = InlineKeyboardButton(str(i),callback_data=str(i))
     Btn.append(x)
-  ak =  = [Btn[i:i+3] for i in range(0, len(Btn), 3)]
+  ak = [Btn[i:i+3] for i in range(0, len(Btn), 3)]
   #ak =  [Btn[i:i+3] for i in range(0,len(l),3)]
 #Out[17]: [[0, 1, 2], [3, 4, 5], [6, 7, 8], [9, 10, 11], [12, 13]]
 #Share Edit Follow
