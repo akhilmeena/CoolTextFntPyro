28df904d889b9136ceb7648648541b935b6abb7f
AKHILESH KUMAR <48666821+akhilmeena@users.noreply.github.com>
2021-11-27 19:13:00 +0530

    update callbackquery.py

--- plugins/callbackquery.py
+++ plugins/callbackquery.py
@@ -15,9 +15,9 @@ logging.getLogger("pyrogram").setLevel(logging.WARNING)
 @Client.on_callback_query()
 async def cb_data(bot, update):
   if update.data == "vsniascrnt":
-    Source_List = currentaffairs.getallmonthfromiasvsncurrentafr()
-    newbtns = currentaffairs.makeBtnFromDict(Source_List)
-    await update.message.edit_text(text="<b>Choose Your Month</b>",reply_markup=newbtns)
+    Source_List = currentaffairs.getallmonthfromiasvsncurrentafr(bot,update)
+    #newbtns = currentaffairs.makeBtnFromDict(Source_List)
+    #await update.message.edit_text(text="<b>Choose Your Month</b>",reply_markup=newbtns)
   if update.data == "crnafrsdaily":
     await update.message.edit_text(text="<b>Choose Your Source</b>",reply_markup=currentaffairs.CRNTAFRSOURCEBTN)
   if update.data == "libraryopen":
