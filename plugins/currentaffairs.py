8acf3cb1d67d38f3dd7a90bc96acbe02ce23a40b
AKHILESH KUMAR <48666821+akhilmeena@users.noreply.github.com>
2021-11-27 14:16:40 +0530

    update currentaffairs.py

--- plugins/currentaffairs.py
+++ plugins/currentaffairs.py
@@ -19,7 +19,9 @@ async def start(bot, message):
   for i in range(10):
     x = InlineKeyboardButton(i,callback_data=i)
     Btn.append(x)
-  ak = list(chunk(Btn, 3))
+  ak = list(list(t) for t in zip(*[iter(btn)]*3))
+  #print(ak)
+  #ak = list(chunk(Btn, 3))
   #[(0, 1, 2), (3, 4, 5), (6, 7, 8), (9, 10, 11), (12, 13)]
   LBRYOPEN_BUTTONS = InlineKeyboardMarkup(ak)
   await message.reply_text(text="akh",reply_markup=LBRYOPEN_BUTTONS)
