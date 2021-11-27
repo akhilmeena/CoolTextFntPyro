102415e699d86d8b5505207dcbc7ca7fc4ba2148
AKHILESH KUMAR <48666821+akhilmeena@users.noreply.github.com>
2021-11-27 14:07:20 +0530

    update currentaffairs.py

--- plugins/currentaffairs.py
+++ plugins/currentaffairs.py
@@ -18,7 +18,7 @@ finalbtn = []
 async def start(bot, message):
   for i in range(10):
     x = InlineKeyboardButton(i,callback_data=i)
-    Btn.append()
+    Btn.append(x)
   ak = list(chunk(Btn, 3))
   #[(0, 1, 2), (3, 4, 5), (6, 7, 8), (9, 10, 11), (12, 13)]
   LBRYOPEN_BUTTONS = InlineKeyboardMarkup(ak)
