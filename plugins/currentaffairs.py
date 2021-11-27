75362b58ff1e034c1d556e8a340fd7e3e07c6901
AKHILESH KUMAR <48666821+akhilmeena@users.noreply.github.com>
2021-11-27 14:03:40 +0530

    update currentaffairs.py

--- plugins/currentaffairs.py
+++ plugins/currentaffairs.py
@@ -1,1 +1,32 @@
+import pyrogram
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardButton
+import string
+from itertools import islice
+
+def chunk(it, size):
+    it = iter(it)
+    return iter(lambda: tuple(islice(it, size)), ())
+
+
+
+
+Btn = []
+finalbtn = []
+
+@Client.on_message(filters.command('btn') & filters.private)
+async def start(bot, message):
+  for i in range(10):
+    x = InlineKeyboardButton(i,callback_data=i)
+    Btn.append()
+  ak = list(chunk(Btn, 3))
+  #[(0, 1, 2), (3, 4, 5), (6, 7, 8), (9, 10, 11), (12, 13)]
+  LBRYOPEN_BUTTONS = InlineKeyboardMarkup(ak)
+  await message.reply_text(text="akh",reply_markup=LBRYOPEN_BUTTONS)
+
+      
+    
+  
+
+  
 
