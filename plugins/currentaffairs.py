8e728fa7421268ca79b95d3800345a4bdea57a81
AKHILESH KUMAR <48666821+akhilmeena@users.noreply.github.com>
2021-11-27 19:01:26 +0530

    update currentaffairs.py

--- plugins/currentaffairs.py
+++ plugins/currentaffairs.py
@@ -38,7 +38,18 @@ def getallmonthfromiasvsncurrentafr():
     addList.append(str(month_num))
     addList.append(str(YearFull))
     addDict["CallBtnData"] = f"{addList}"
-  return Source_List
+  #return Source_List
+  Btn = []
+  for d in Source_Dict:
+    print(d)
+    CallbackText = d['CallBtnTedt']
+    CallbackData = d['CallBtnData']
+    x = InlineKeyboardButton(str(CallbackText),callback_data=CallbackData)
+    Btn.append(x)
+  ak = [Btn[i:i+3] for i in range(0, len(Btn), 3)]
+  newbtns = InlineKeyboardMarkup(ak)
+  return newbtns
+
   
 
 
