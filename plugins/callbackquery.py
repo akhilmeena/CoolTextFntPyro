8af806acafe862a79e8dfbe372e2ad7781c5bea8
AKHILESH KUMAR <48666821+akhilmeena@users.noreply.github.com>
2021-11-28 17:55:05 +0530

    update callbackquery.py

--- plugins/callbackquery.py
+++ plugins/callbackquery.py
@@ -18,7 +18,7 @@ async def cb_data(bot, update):
   if (update.data.startswith("['crnttodayvsnias'")):
     await update.message.edit_text(text=update.data)
     getcodeheadwithday = ast.literal_eval(update.data)[1]
-    await message.reply_text(text="akhil")
+    await update.reply_text(text="akhil")
     print(getcodeheadwithday)
   if (update.data.startswith("['getcurrentofmonthvsnias'")):
     month_num = ast.literal_eval(update.data)[1]
