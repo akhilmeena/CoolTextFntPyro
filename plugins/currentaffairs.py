90293b468ade48d1a16f401c348204cebdc15cd0
AKHILESH KUMAR <48666821+akhilmeena@users.noreply.github.com>
2021-11-28 10:37:38 +0530

    update currentaffairs.py

--- plugins/currentaffairs.py
+++ plugins/currentaffairs.py
@@ -75,7 +75,7 @@ def currentdaypdfbuttonvsnias(month_num,year_num):
     getpdfcodehead = incpltDatestrng2[0:5]
     incpltDatestrng1 = incpltDatestrng2.replace(f"{getpdfcodehead}-","")
     incpltDatestrng = incpltDatestrng1.replace(".pdf","")
-    addList = ["downldcrntofdayvsnias"]
+    addList = ["crnttodayvsnias"]
     addList.append(f"{getpdfcodehead}-{incpltDatestrng}")
     #addList.append(str(incpltDatestrng))
     addDict["CallBtnTedt"] = str(incpltDatestrng)
