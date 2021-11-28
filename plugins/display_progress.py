7ab6a338b71633c974d2829c656ddb8e2b56383a
AKHILESH KUMAR <48666821+akhilmeena@users.noreply.github.com>
2021-11-28 18:47:39 +0530

    update display_progress.py

--- plugins/display_progress.py
+++ plugins/display_progress.py
@@ -2,7 +2,7 @@
 
 import math
 import time
-from helpers.configs import Config
+from plugins.helper import Config
 
 
 async def progress_for_pyrogram(current, total, ud_type, message, start):
@@ -14,15 +14,13 @@ async def progress_for_pyrogram(current, total, ud_type, message, start):
         elapsed_time = round(diff) * 1000
         time_to_completion = round((total - current) / speed) * 1000
         estimated_total_time = elapsed_time + time_to_completion
-
         elapsed_time = TimeFormatter(milliseconds=elapsed_time)
         estimated_total_time = TimeFormatter(milliseconds=estimated_total_time)
-
         progress = "[{0}{1}] \n".format(
             ''.join(["●" for i in range(math.floor(percentage / 5))]),
             ''.join(["○" for i in range(20 - math.floor(percentage / 5))])
         )
-
+        
         tmp = progress + Config.PROGRESS.format(
             round(percentage, 2),
             humanbytes(current),
