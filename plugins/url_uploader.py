598be9266cfedd134b45ccdea395ca3176cf9c32
AKHILESH KUMAR <48666821+akhilmeena@users.noreply.github.com>
2021-11-28 20:32:03 +0530

    update url_uploader.py

--- plugins/url_uploader.py
+++ plugins/url_uploader.py
@@ -50,6 +50,7 @@ async def leecher2(bot , update,Url):
   except:
     msg = await update.message.reply_text(text=f"`Analyzing Your Link ...`", quote=True)
   file_path = os.path.join(mainpath, os.path.basename(url))
+  filename = os.path.join(mainpath, os.path.basename(url))
   filename3 = url.split('/')[-1]
   filename2 = filename3.replace('%25','_')
   filename1 = filename2.replace(' ','_')
