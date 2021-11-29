dbaf78abcac4e98fa6a300bff33cfae8e0e4cfdd
AKHILESH KUMAR <48666821+akhilmeena@users.noreply.github.com>
2021-11-29 16:27:09 +0530

    update urluploader.py

--- plugins/urluploader.py
+++ plugins/urluploader.py
@@ -3,6 +3,8 @@ import logging
 import os
 from config import Config
 import pyrogram
+from plugins import helper
+import requests
 from pyrogram import Client, filters
 
 logging.basicConfig(level=logging.DEBUG,
@@ -17,10 +19,6 @@ async def Find(string):
   url = re.findall(regex,string)       
   return [x[0] for x in url] 
 
-
-
-
-
 async def Urlleaccher(bot,update,Url2Dowload):
   msg = await update.message.reply_text("Validating Url...")
   try:
@@ -29,7 +27,30 @@ async def Urlleaccher(bot,update,Url2Dowload):
     msg = await update.message.reply_text("Couldn't Found Url in Given Text...")
     return
   msg = await msg.edit("Url Matched : {}".format(url),disable_web_page_preview=True)
+  directory = f"{update.chat.id}"
+  print(directory)
+  parent_dir = "DownloadPdf/"
+  path = os.path.join(parent_dir, directory) 
+  isExist = os.path.exists(path)
+  if not isExist:
+    os.mkdir(path)
+  else:
+    os.remove(path)
+    os.mkdir(path)
+  msg = await msg.edit("Url Matched : {}".format(url),disable_web_page_preview=True)
+  file_name = url.split('/')[-1]
+  file_path = os.path.join(path, file_name)
+  response = requests.get(urlforpdf)
+  total_length = int(response.headers["Content-Length"])
+  content_type = response.headers["Content-Type"]
+  msg = await msg.edit(helper.DonloadFiletext.format(url,total_length,file_path,content_type),disable_web_page_preview=True)
   
+        
+  
+  
+  
+    prpgressmsg = bot.send_message(chat_Id,text="📥 Trying to Download...",parse_mode="HTML")
+    try:
   
   
   
