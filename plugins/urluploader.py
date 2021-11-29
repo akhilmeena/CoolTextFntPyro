f654fa7eeaeb93c36ca60d4ba608732b5d528fee
AKHILESH KUMAR <48666821+akhilmeena@users.noreply.github.com>
2021-11-29 14:11:20 +0530

    update urluploader.py

--- plugins/urluploader.py
+++ plugins/urluploader.py
@@ -1,1 +1,35 @@
+import re 
+import logging
+import os
+from config import Config
+import pyrogram
+from pyrogram import Client, filters
 
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+
+
+
+async def Find(string): 
+  regex = r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))"
+  url = re.findall(regex,string)       
+  return [x[0] for x in url] 
+
+
+
+
+
+async def Urlleaccher(bot,update,Url2Dowload):
+  msg = await update.message.reply_text("Validating Url...")
+  LinkValid = False
+  try:
+    url = await Find(Url2Dowload)
+    LinkValid = True
+  except:
+    if LinkValid is False:
+      await msg.edit("Link is Invalid")
+    break
+  
+  
