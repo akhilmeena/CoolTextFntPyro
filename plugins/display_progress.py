3efe5a495f2eaca5dcb875e5814c887960759ddc
AKHILESH KUMAR <48666821+akhilmeena@users.noreply.github.com>
2021-11-30 14:06:30 +0530

    update display_progress.py

--- plugins/display_progress.py
+++ plugins/display_progress.py
@@ -31,10 +31,9 @@ async def progress_for_pyrogram(current, total, ud_type, message, start):
         elapsed_time = TimeFormatter(milliseconds=elapsed_time)
         estimated_total_time = TimeFormatter(milliseconds=estimated_total_time)
 
-        progress = "[{0}{1}] \n<b>🗂️ File Name </b>:<code>{}</code>\n <b>📊Percentage:</b> {2}%\n".format(
+        progress = "[{0}{1}] \n<b>🗂️ File Name </b>:<code>akh</code>\n <b>📊Percentage:</b> {2}%\n".format(
             ''.join(["■" for i in range(math.floor(percentage / 5))]),
             ''.join(["□" for i in range(20 - math.floor(percentage / 5))]),
-            file_name,
             round(percentage, 2))
 
         tmp = progress + "<b>✅Completed:</b>{0} \n<b>📁Total Size:</b> {1}\n<b>🚀Speed:</b> {2}/s\n<b>⌚️ETA:</b> {3}\n".format(
