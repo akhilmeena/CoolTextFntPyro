c0b17c732d3dc175d71b2984cb202cc3d272a4d6
AKHILESH KUMAR <48666821+akhilmeena@users.noreply.github.com>
2021-11-30 14:08:57 +0530

    update urluploader.py

--- plugins/urluploader.py
+++ plugins/urluploader.py
@@ -113,6 +113,7 @@ async def Urlleaccher(bot,update,Url2Dowload):
         progress=progress_for_pyrogram,
         progress_args=(
           "<b>File is Uploading ⌛</b>",
+          file_path,
           msg, 
           c_time
           )
