8e403d3914e594b52d1c8a35a4d99306d63e5031
AKHILESH KUMAR <48666821+akhilmeena@users.noreply.github.com>
2021-12-05 11:54:59 +0530

    update Newspapers.py

--- plugins/Newspapers.py
+++ plugins/Newspapers.py
@@ -1,1 +1,14 @@
 import json
+import pyrogram
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+
+NewsPapers = InlineKeyboardButton('The Hindu', callback_data='thehindu')
+HomeToStart = InlineKeyboardButton('🔙', callback_data='libraryopen')
+
+
+NewspaperType = InlineKeyboardMarkup([
+  [NewsPapers],
+  [HomeToStart]
+  ])
