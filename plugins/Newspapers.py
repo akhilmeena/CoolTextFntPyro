7ed7289ae6e8ff24e1949d51f1414a0854c384c8
AKHILESH KUMAR <48666821+akhilmeena@users.noreply.github.com>
2021-12-05 16:21:18 +0530

    update Newspapers.py

--- plugins/Newspapers.py
+++ plugins/Newspapers.py
@@ -2,7 +2,9 @@ import json
 import pyrogram
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-
+import requests
+from bs4 import BeautifulSoup
+import re
 
 NewsPapers = InlineKeyboardButton('The Hindu', callback_data='thehindu')
 HomeToStart = InlineKeyboardButton('🔙', callback_data='libraryopen')
@@ -12,3 +14,37 @@ NewspaperType = InlineKeyboardMarkup([
   [NewsPapers],
   [HomeToStart]
   ])
+
+
+############## THE HINDU NEWSPAPER FUNCITON START ############## 
+
+TheHindu30Result = []
+
+
+async def gettingAllHinduresult(bot,update):
+  c = 0
+  url="https://dailyepaper.in/the-hindu-pdf-free-download-04-dec-2021/"
+  response=requests.get(url)
+  data = response.text
+  htmlParse = BeautifulSoup(data, 'html.parser') 
+  for para in htmlParse.find_all("p"): 
+    if c =< 30:
+      fullstring = f"{para}"
+      substring = "https://vk.com/"
+      if substring in fullstring:
+        #akh = re.findall(r'(https?://[^\s]+)', fullstring)
+        msg = await update.message.reply_text(fullstring)
+        c+=1
+        #print(para.text)
+      else:
+        pass
+    else:
+      break
+    #print(akh)
+    #['http://www.google.com', 'http://stackoverflow.com/questions/839994/extracting-a-url-in-python']
+    
+      #htmlParse = BeautifulSoup(data, 'html.parser') 
+
+
+
+############## THE HINDU NEWSPAPER FUNCITON END ############## 
