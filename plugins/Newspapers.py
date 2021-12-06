107866a80118ef7eca6da2b5f171759676616108
AKHILESH KUMAR <48666821+akhilmeena@users.noreply.github.com>
2021-12-06 13:47:19 +0530

    update Newspapers.py

--- plugins/Newspapers.py
+++ plugins/Newspapers.py
@@ -41,13 +41,13 @@ async def gettingAllHinduresult(bot,update):
         linklist = re.findall(r'(https?://[^\s]+)', fullstring)
         datebtn = InlineKeyboardButton(f"📆 {itmelist[0]}", callback_data="nothing")
         try:
-          dwnldbtn = InlineKeyboardButton(f"📥 {itmelist[1]}", callback_data="['dwnldnewspaper" + ',' + "NP"  + ',' + str(c+1) + "']")
+          dwnldbtn = InlineKeyboardButton(f"📥 {itmelist[1]}", callback_data="['dwnldnewspaper" + "','" + "NP"  + "','" + str(c+1) + "']")
         except:
-          dwnldbtn = InlineKeyboardButton(f"N/A", callback_data="['dwnldnewspaper" + ',' + "NP" + ',' + str(c+1) + "']")
+          dwnldbtn = InlineKeyboardButton(f"N/A", callback_data="['dwnldnewspaper" + "','" + "NP" + "','" + str(c+1) + "']")
         try:
-          anylsisbtn = InlineKeyboardButton(f"📄 {itmelist[2]}", callback_data="['dwnldnewspaper" + ',' + "AL"  + ',' + str(c+1) + "']")
+          anylsisbtn = InlineKeyboardButton(f"📄 {itmelist[2]}", callback_data="['dwnldnewspaper" + "','" + "AL"  + "','" + str(c+1) + "']")
         except:
-          anylsisbtn = InlineKeyboardButton(f"N/A", callback_data="['dwnldnewspaper" + ',' + "AL"  + ',' + str(c+1) + "']")
+          anylsisbtn = InlineKeyboardButton(f"N/A", callback_data="['dwnldnewspaper" + "','" + "AL"  + "','" + str(c+1) + "']")
         btn.append(datebtn)
         btn.append(dwnldbtn)
         btn.append(anylsisbtn)
