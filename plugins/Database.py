75d619934c634609ddb0db388b7065f91c52ac97
AKHILESH KUMAR <48666821+akhilmeena@users.noreply.github.com>
2021-12-11 04:47:38 +0530

    update Database.py

--- plugins/Database.py
+++ plugins/Database.py
@@ -18,3 +18,16 @@ creds = ServiceAccountCredentials.from_json_keyfile_name("credential.json", scop
 client = gspread.authorize(creds)
 ak = client.open("LibraryData")
 UserData = ak.worksheet("User")
+
+
+async def AddNewUser(UserID):
+  cells = UserData.findall(UserID)
+  if len(cells) > 0:
+    return
+  else:
+    h = UserData.get('A1000').first()
+    h1 = int(h) + 1
+    UserData.update_cell(int(h1),1 ,f"{h1}")
+    UserData.update_cell(int(h1),2 ,id2)
+    return
+ 
