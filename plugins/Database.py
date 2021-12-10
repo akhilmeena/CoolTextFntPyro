92b5081e8f5c588371f8521d5628ec9856527f2b
AKHILESH KUMAR <48666821+akhilmeena@users.noreply.github.com>
2021-12-11 03:38:47 +0530

    update Database.py

--- plugins/Database.py
+++ plugins/Database.py
@@ -1,1 +1,20 @@
+import logging
+import os
+from config import Config
+import pyrogram
+from pyrogram import Client
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
 
+
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+
+
+scope = ["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/spreadsheets',"https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
+creds = ServiceAccountCredentials.from_json_keyfile_name("credential.json", scope)
+client = gspread.authorize(creds)
+ak = client.open("LibraryData")
+UserData = ak.worksheet("User")
