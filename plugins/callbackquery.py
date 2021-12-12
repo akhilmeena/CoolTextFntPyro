eb26088d1de55167b500ff070e596bc8d6a3663b
AKHILESH KUMAR <48666821+akhilmeena@users.noreply.github.com>
2021-12-13 00:01:36 +0530

    update callbackquery.py

--- plugins/callbackquery.py
+++ plugins/callbackquery.py
@@ -25,11 +25,13 @@ async def cb_data(bot, update):
   if (update.data.startswith("['dwnldnewspaper'")):
     Id = ast.literal_eval(update.data)[1]
     Forwhat = ast.literal_eval(update.data)[2]
-    print(Forwhat)
     try:
       Textfornewspaperwithanylss = await Newspapers.captionfornewslink(Id,Forwhat)
     except:
-      Textfornewspaperwithanylss = await Newspapers.captionfornewslink1(Id,Forwhat)
+      if str(Forwhat) == "":
+        Textfornewspaperwithanylss = await Newspapers.captionfornewslinkdainikjagaran(Id,Forwhat)
+      else:
+        Textfornewspaperwithanylss = await Newspapers.captionfornewslink1(Id,Forwhat)
     newbtns = InlineKeyboardMarkup([[InlineKeyboardButton("🔙",callback_data=Forwhat)]])
     await update.message.edit_text(text=Textfornewspaperwithanylss,reply_markup=newbtns)
   if (update.data.startswith("['dwldboobsncert'")):
