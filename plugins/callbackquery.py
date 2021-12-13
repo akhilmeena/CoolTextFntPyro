124b6a769f3102a9de66dbc459c92207811e58a5
AKHILESH KUMAR <48666821+akhilmeena@users.noreply.github.com>
2021-12-13 06:43:54 +0530

    update callbackquery.py

--- plugins/callbackquery.py
+++ plugins/callbackquery.py
@@ -65,6 +65,11 @@ async def cb_data(bot, update):
     Source_List = await Newspapers.gettingallDainikJagranresult(bot,update)
     newbtns = await Newspapers.makeBtnFromDict(Source_List)
     await update.message.edit_text(text=f"<b>Choose Date.</b>\n{Newspapers.DisclaimerForAll}",reply_markup=newbtns)
+  if update.data == "economictimes":
+    await update.answer(text = Newspapers.EconomicTimesNotification, show_alert=True)
+    Source_List = await Newspapers.gettingallEconomictimesresult(bot,update)
+    newbtns = await Newspapers.makeBtnFromDict(Source_List)
+    await update.message.edit_text(text=f"<b>Choose Date.</b>\n{Newspapers.DisclaimerForAll}",reply_markup=newbtns)
   if update.data == "financialexpress":
     await update.answer(text = Newspapers.FinancialExpressNotification, show_alert=True)
     Source_List = await Newspapers.gettingallFinancialExpressresult(bot,update)
