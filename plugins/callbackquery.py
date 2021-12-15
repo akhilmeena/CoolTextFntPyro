739b73beff32e4eb74c617346653635f4cbed4f0
AKHILESH KUMAR <48666821+akhilmeena@users.noreply.github.com>
2021-12-15 20:41:09 +0530

    update callbackquery.py

--- plugins/callbackquery.py
+++ plugins/callbackquery.py
@@ -40,24 +40,21 @@ async def cb_data(bot, update):
     #print(SShotName)
     msg = update.message.reply_text("ak")
     akhil =  open(mfile_path, 'rb')
+    c_time = time.time()
     await bot.send_document(
       chat_id=CHAT_ID,
-      document=akhil,
-      c_time = time.time()
-      await bot.send_document(
-        chat_id=CHAT_ID,
-        document=doc,
-        file_name=newFileName,
-        thumb=thumb_image_path,
-        force_document=True,
-        caption=f"<b>{file_name}</b>",
-        progress=progress_for_pyrogram,
-        progress_args=(
-          f"<b>File is Uploading ⌛</b>\n\n<b>🗂️ File Name :</b> <code>{newFileName}</code>",
-          msg, 
-          c_time
-          )
+      document=doc,
+      file_name=newFileName,
+      thumb=thumb_image_path,
+      force_document=True,
+      caption=f"<b>{file_name}</b>",
+      progress=progress_for_pyrogram,
+      progress_args=(
+        f"<b>File is Uploading ⌛</b>\n\n<b>🗂️ File Name :</b> <code>{newFileName}</code>",
+        msg, 
+        c_time
         )
+      )
   if update.data == "chahalacdmy":
     Source_List = await currentaffairs.getalldateswithlinkfromchahalacadmy(bot,update)
     newbtns = currentaffairs.makeBtnFromDict(Source_List)
