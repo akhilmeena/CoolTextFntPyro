73a5e4d5508bfbce249997ba8f62a6f5e471c7c4
AKHILESH KUMAR <48666821+akhilmeena@users.noreply.github.com>
2021-12-16 14:44:55 +0530

    update callbackquery.py

--- plugins/callbackquery.py
+++ plugins/callbackquery.py
@@ -31,7 +31,9 @@ async def cb_data(bot, update):
     Id = ast.literal_eval(update.data)[1]
     MagziCompany = ast.literal_eval(update.data)[2]
     Month,LinkMagzine,Lang = await Magzines.GetLinkDateLang(Id,MagziCompany)
-    await update.message.reply_text(f"<b>{Month}\n{LinkMagzine}\n{Lang}</b>")
+    Url2Dowload = f"{LinkMagzine}".replace(" ","%20")
+    await urluploader.Urlleaccher(bot,update,Url2Dowload)
+    #await update.message.reply_text(f"<b>{Month}\n{LinkMagzine}\n{Lang}</b>")
   if (update.data.startswith("['Chlacdmycrntafrmagzine'")):
     Lang = ast.literal_eval(update.data)[1]
     Data = await Magzines.getDataChahalMagzResult(bot,update,Lang)
