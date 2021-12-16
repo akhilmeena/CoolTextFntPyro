d6ae7b06bd4a62240d87347550185da555b3af35
AKHILESH KUMAR <48666821+akhilmeena@users.noreply.github.com>
2021-12-16 10:00:37 +0530

    update callbackquery.py

--- plugins/callbackquery.py
+++ plugins/callbackquery.py
@@ -27,6 +27,9 @@ logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 @Client.on_callback_query()
 async def cb_data(bot, update):
+  if (update.data.startswith("['Chlacdmycrntafrmagzine'")):
+    Lang = ast.literal_eval(update.data)[1]
+    Source_List = await getAllChahalMagzResult(bot,update,Lang)
   if update.data == "magzines":
     await update.message.edit_text(text="<b>🎯 Choose Your Magzines To Download</b>",reply_markup=Magzines.MagzinesType)
   if (update.data.startswith("['indxchlacdmy'")):
