928fa6f88c262849b43e349b95899caaecaa38c3
AKHILESH KUMAR <48666821+akhilmeena@users.noreply.github.com>
2021-12-16 08:47:36 +0530

    update Magzines.py

--- plugins/Magzines.py
+++ plugins/Magzines.py
@@ -11,23 +11,20 @@ logger = logging.getLogger(__name__)
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 
-ChlacdmycrntafrmagzineHindi = InlineKeyboardButton('Chahal Acdmy Crnt Afrs(Hindi)', callback_data="['Chlacdmycrntafrmagzine','Hindi']")
-ChlacdmycrntafrmagzineEnglish = InlineKeyboardButton('Chahal Acdmy Crnt Afrs(English)', callback_data="['Chlacdmycrntafrmagzine','English']")
-YoznaMagzineHindi = InlineKeyboardButton('Yojana Monthly Magazines(Hindi)', callback_data="['YoznaMagzine','Hindi']")
-YoznaMagzineEnglish = InlineKeyboardButton('Yojana Monthly Magazines(English)', callback_data="['YoznaMagzine','English']")
-KurukshetraMagzineHindi = InlineKeyboardButton('Kurukshetra Monthly Magazines(Hindi)', callback_data="['KurukshetraMagzine','Hindi']")
-KurukshetraMagzineEnglish = InlineKeyboardButton('Kurukshetra Monthly Magazines(English)', callback_data="['KurukshetraMagzine','English']")
+ChlacdmycrntafrmagzineHindi = InlineKeyboardButton('Chahal (Hindi)', callback_data="['Chlacdmycrntafrmagzine','Hindi']")
+ChlacdmycrntafrmagzineEnglish = InlineKeyboardButton('Chahal (English)', callback_data="['Chlacdmycrntafrmagzine','English']")
+YoznaMagzineHindi = InlineKeyboardButton('Yojana (Hindi)', callback_data="['YoznaMagzine','Hindi']")
+YoznaMagzineEnglish = InlineKeyboardButton('Yojana (English)', callback_data="['YoznaMagzine','English']")
+KurukshetraMagzineHindi = InlineKeyboardButton('Kurukshetra (Hindi)', callback_data="['KurukshetraMagzine','Hindi']")
+KurukshetraMagzineEnglish = InlineKeyboardButton('Kurukshetra (English)', callback_data="['KurukshetraMagzine','English']")
 #"['KurukshetraMagzine','Hindi']"
 #"['KurukshetraMagzine','English']"
 HomeToStart = InlineKeyboardButton('🔙', callback_data='libraryopen')
 
 
 MagzinesType = InlineKeyboardMarkup([
-  [ChlacdmycrntafrmagzineHindi],
-  [ChlacdmycrntafrmagzineEnglish],
-  [YoznaMagzineHindi],
-  [YoznaMagzineEnglish],
-  [KurukshetraMagzineHindi],
-  [KurukshetraMagzineEnglish],
+  [ChlacdmycrntafrmagzineEnglish,ChlacdmycrntafrmagzineHindi],
+  [YoznaMagzineEnglish,YoznaMagzineHindi],
+  [KurukshetraMagzineEnglish,KurukshetraMagzineHindi],
   [HomeToStart]
   ])
