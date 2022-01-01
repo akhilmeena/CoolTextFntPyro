798e79e79c451799a3487b7d4677813d5310f6cf
AKHILESH KUMAR <48666821+akhilmeena@users.noreply.github.com>
2022-01-01 14:29:25 +0530

    update Inline_Query.py

--- plugins/Inline_Query.py
+++ plugins/Inline_Query.py
@@ -8,14 +8,17 @@ from pyrogram.types import (InlineQuery, InlineQueryResultArticle, InlineQueryRe
 @Client.on_inline_query()
 async def inline(bot, inline_query):
   string = inline_query.query.lower()
-  
+  CHAT_ID = inline_query.message.chat.id
   if string == "":
     await inline_query.answer(
       results=[
         InlineQueryResultArticle(
           title="🔄 Share With Others",
           input_message_content=InputTextMessageContent(
-            "<b>I Invite You to join a LIBRARY BOT 🏦. Here you can search 🔍 Any Type of reading materials...📖 </b><i>Eg. Textbooks 📚, Novels 📗, Daily Newspapers🗞️ , magazines📑 , Current Affairs,UPSC/PSC NOTES Etc</i>"
+            """<b>I Invite You to join a LIBRARY BOT 🏦. Here you can search 🔍 Any Type of reading materials...📖 </b>
+<i>Eg. Textbooks 📚, Novels 📗, Daily Newspapers🗞️ , magazines📑 , Current Affairs,UPSC/PSC NOTES Etc</i>
+
+<b>🔗 Referral Link : https://t.me/LibraryInbot?start={}""".format(CHAT_ID)
             ),
             url="https://t.me/Libraryinbot",
             description="Tap To Share",
