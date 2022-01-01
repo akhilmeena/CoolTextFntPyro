2dfb215c06321e02a527f8b7170ef1ab1bc6b859
AKHILESH KUMAR <48666821+akhilmeena@users.noreply.github.com>
2022-01-01 15:43:37 +0530

    update Wallet.py

--- plugins/Wallet.py
+++ plugins/Wallet.py
@@ -11,11 +11,11 @@ import re
 
 AccntDetails = """<b>💼 Account Details:
  ===========================
-💰 Balance : </b><code>{} Coins</code>
-<b>📨 Invited : </b><code>{} Users</code>
+💰 Balance : </b><code> {} Coins</code>
+<b>📨 Invited : </b><code> {} Users</code>
 <b>📡 Refer & Earn : https://t.me/LibraryInbot?start={}
 
 ◆ <i>Per Refer you will get 600 Free Coins Or Else You can Recharge Your account through Buying Coints @ 500 Coins/1 Rs.
 
-💝 Account Balance for Super user will be INFINITE COINS. To become Super User You must invite atleast 10 Users.
-<i></b>"""
+💝 <u>Account Balance for Super user will be INFINITE COINS. To become Super User You must invite atleast 10 Users.
+</u></i></b>"""
