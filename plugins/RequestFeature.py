3952cf53b34e2ea7a68aa7cf04d86be1eff01762
AKHILESH KUMAR <48666821+akhilmeena@users.noreply.github.com>
2022-01-02 08:35:28 +0530

    update RequestFeature.py

--- plugins/RequestFeature.py
+++ plugins/RequestFeature.py
@@ -1,1 +1,21 @@
+import logging
+import os
+from config import Config
+import pyrogram
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+
+
+#Request Newspaper 
+@Client.on_callback_query()
+async def cb_data(bot, update):
+  if update.data == "requestnewspaper":
+    await update.message.reply_text(f"Good")
+
+
 
