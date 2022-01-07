c84cb06cacd9e54abc68efaef1a0e61db7a7acb6
AKHILESH KUMAR <48666821+akhilmeena@users.noreply.github.com>
2022-01-07 15:54:53 +0530

    update TextHandler.py

--- plugins/TextHandler.py
+++ plugins/TextHandler.py
@@ -29,7 +29,7 @@ async def NewspaperName(bot, message):
     #)
 
 #@Client.on_message(filters.private & filters.reply & filters.text)
-@Client.on_message(filters.private & filters.text)
+#@Client.on_message(filters.private & filters.text)
 async def TextCheck(bot,message):
   print("akhil")
 
