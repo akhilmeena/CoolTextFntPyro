bf069abb2b417fd763c235676f7458acb4bde3f0
AKHILESH KUMAR <48666821+akhilmeena@users.noreply.github.com>
2022-01-13 13:59:00 +0530

    update Books.py

--- plugins/Books.py
+++ plugins/Books.py
@@ -1,1 +1,12 @@
+import logging
+import os
+from config import Config
+import pyrogram
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
