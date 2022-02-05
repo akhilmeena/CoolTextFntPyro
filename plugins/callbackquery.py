27d03ab1d4432b3967572de5531e5d457f563a0c
AKHILESH KUMAR <48666821+akhilmeena@users.noreply.github.com>
2022-02-05 23:11:58 +0530

    update callbackquery.py

--- plugins/callbackquery.py
+++ plugins/callbackquery.py
@@ -24,7 +24,6 @@ async def cb_data(bot, update):
   if update.data == "abtdvlngbot":
     await update.message.edit_text(text=helper.BotAboutText.format(update.message.from_user.mention),reply_markup=helper.DVLGBTN)
   if update.data == "homeTostart":
-    print(update.message.chat.mention)
     await update.message.edit_text(text=helper.STARTText.format(update.message.chat.first_name),reply_markup=helper.HOME_PAGE)
   if update.data == "MoreBots":
     await update.message.edit_text(text=helper.MoreBotsText.format(update.message.from_user.mention),reply_markup=helper.MoreBots_BTN)
