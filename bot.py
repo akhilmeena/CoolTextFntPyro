e49a5297a7b73a71bc047538a1721f3c0acbe1c9
AKHILESH KUMAR <48666821+akhilmeena@users.noreply.github.com>
2022-02-05 16:54:43 +0530

    update bot.py

--- bot.py
+++ bot.py
@@ -11,12 +11,9 @@ logger = logging.getLogger(__name__)
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 if __name__ == "__main__" :
-  download_path = "Downloads/"
-  if not os.path.isdir(download_path):
-    os.mkdir(download_path)
   plugins = dict(root="plugins")
   app = Client(
-    "LibraryBot",
+    "CoolTextFonts",
     bot_token=Config.BOT_TOKEN,
     api_id=Config.API_ID,
     api_hash=Config.API_HASH,
