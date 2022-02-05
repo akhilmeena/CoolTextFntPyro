8032b22674afebadeff1527ee26fbbbe65456ec1
AKHILESH KUMAR <48666821+akhilmeena@users.noreply.github.com>
2022-02-05 16:53:40 +0530

    update config.py

--- config.py
+++ config.py
@@ -1,10 +1,9 @@
 import os
 
 class Config(object):
-    BOT_TOKEN = "2139115252:AAEYMEKzh33CGCqOxrq0sQTARf4-HgOkSjo"#os.environ.get("BOT_TOKEN", "")
+    BOT_TOKEN = "2109918986:AAEbptM1hAQ6fNz70fa4tov3HavfbLDS-1U"#os.environ.get("BOT_TOKEN", "")
     API_ID = 1687826#int(os.environ.get("APP_ID", ""))
     API_HASH = "1af0252a7f97e288e2fff6b0adad5b2f"#os.environ.get("API_HASH", "")
     OWNER_ID = [818396979]
     MaintainaceYN = ["No"]
-    LoGoPath = "Picsart_21-12-28_23-07-49-780.jpg"
     Bot_Username = "@LibraryInBot"
