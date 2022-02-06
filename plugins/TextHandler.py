c91580aaa2f185cc5324d40449d0a15437912734
AKHILESH KUMAR <48666821+akhilmeena@users.noreply.github.com>
2022-02-06 10:04:37 +0530

    update TextHandler.py

--- plugins/TextHandler.py
+++ plugins/TextHandler.py
@@ -12,11 +12,11 @@ logger = logging.getLogger(__name__)
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 
-@Client.on_message(filters.text & ~filters.command)
+#@Client.on_message(filters.text & ~filters.command)
 def my_handler(client, message):
     print(message)
 
-#@Client.on_message(filters.private & ~filters.command & filters.text)
+@Client.on_message(filters.private & ~filters.command() & filters.text)
 async def TextHandlewithFont(bot,message):
   await message.reply_text("Choose Your Methods",reply_markup=helper.STARTFontingBTN)
 
