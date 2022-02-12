d3f4b4be289893cc27b6202fee04eb1c55bd56db
AKHILESH KUMAR <48666821+akhilmeena@users.noreply.github.com>
2022-02-12 16:02:28 +0530

    update callbackquery.py

--- plugins/callbackquery.py
+++ plugins/callbackquery.py
@@ -53,12 +53,14 @@ async def cb_data(bot, update):
     DesignNumber = ast.literal_eval(update.data)[1]
     #Page_No = ast.literal_eval(update.data)[2]
     Designkeyboard = await TextDecorator.GenerateButtonForDecorate()
-    if len(TextHandler.Current_Text) ==1:
+    TextToChange = TextHandler.GetCurrentTextToStyle()
+    if str(TextToChange) =="Nonee":
+      #TextHandler.Current_Text.clear()
+      await update.message.reply_text("<b>Send Some Text</b>")
+      await update.message.delete()
+    else:
       TextToChange = TextHandler.Current_Text[0]
       TextWithFont = await TextDecorator.DesignWithText(TextToChange,DesignNumber)
       await update.message.edit_text(text=f"{TextWithFont}",reply_markup=Designkeyboard)
-    else:
-      TextHandler.Current_Text.clear()
-      await update.message.reply_text("<b>Send Some Text</b>")
-      await update.message.delete()
+      
   
